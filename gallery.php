cd0c5ef8d02d9f1ce7c1da7205d08d2fabd18f06
Adam <contact@amnsoftware.co.uk>
2018-11-17 16:38:50 +0000

    Added gallery page

---
+++ gallery.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+?>
+
+<?php $currentPage = 'gallery'; ?>
+
+<html>
+	<head>
+		<title>Exhibition Showcase</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        
+	</head>
+	<body class="is-preload homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+				<?php					
+					include "header.php";
+					?>
+				</div>
+
+			<!-- Banner -->
+				<div id="banner-wrapper">
+					<div id="banner" class="box container">
+                        <h2>Gallery Name</h2>
+					</div>
+				</div>
+
+			<!-- Features -->
+				<div id="features-wrapper">
+					<div class="container">
+                        <div class="GalleryViewer">
+
+                        </div>
+					</div>
+				</div>
+
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div class="container">
+						<div class="row gtr-200">
+
+								<!-- Content -->
+									<div id="content">
+										<section class="last">
+											<h2>So what's this all about?</h2>
+											<p><strong>Exhibition Showcase</strong>, a feature full interactive virtual gallery by <a href="http://amnsoftware.co.uk">AMN SOFTWARE</a>.
+											Exhibition Showcase provides the perfect place for all artists and creative people alike to show thier work to the world, while perfect for professional use we encourage students to use this brilliant platform to host thier work for the world and potetial employers to see!
+											</p>
+											<p>.</p>
+											<a href="#" class="button icon fa-arrow-circle-right">Continue Reading</a>
+										</section>
+									</div>
+
+							
+						</div>
+					</div>
+				</div>
+
+			<?php include 'footer.php'; ?>
+
+			</div>
+
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
+
+
+
