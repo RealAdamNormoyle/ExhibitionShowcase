a120b512d4d285bb091c60cc10ac83919b577d88
Adam <contact@amnsoftware.co.uk>
2018-11-17 16:39:58 +0000

    Added gallery setup modules

--- gallery-images-10.php
+++ gallery-images-10.php
@@ -36,7 +36,7 @@ if($rows != 0){
 
 <?php
 
-for($i =0;$i < ($_SESSION['gallerySize']/2);$i++){
+for($i =0;$i < (5);$i++){
 
     $j = $i * 2;
     echo "<div><div style=\"width:40%;float:left;\"><p style=\"font-size:25px;font-weight:bold; margin:3px;float:left;\">".($j+1)."</p><select name=\"ImagePlacement".($j+1)."\"style=\"float:left;\">".$htmlString."</select></div><div style=\"width:40%;float:right;\"><p style=\"float:left;font-weight:bold; font-size:25px;margin:3px;\">".($j+2)."</p><select style=\"float:left;\" name=\"ImagePlacement".($j+2)."\">".$htmlString."</select></div></div>";
